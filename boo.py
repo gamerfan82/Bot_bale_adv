a794b8db464dbaf1ca051dc889f89af68475dd9f
gamerfan82 <83762731+gamerfan82@users.noreply.github.com>
2026-05-23 00:05:47 +0330

    Update boo.py

--- boo.py
+++ boo.py
@@ -2,36 +2,64 @@ import requests
 
 BOT_TOKEN = "900766790:3zB3PpKNztdoPoON6STdUjprXgEECAMHYso"
 
-
-from flask import Flask, request
+import time
+FILE_PATH = "test.jpg"
 
 BASE_URL = f"https://tapi.bale.ai/bot{BOT_TOKEN}"
 
-app = Flask(__name__)
+def get_updates(offset=None):
+    url = f"{BASE_URL}/getUpdates"
+    params = {
+        "offset": offset,
+        "timeout": 30
+    }
+    try:
+        r = requests.get(url, params=params)
+        r.raise_for_status()
+        return r.json()
+    except Exception as e:
+        print("Error in get_updates:", e)
+        return {"result": []}
 
-def send_photo(chat_id, file_path="test.jpg", caption=""):
+def send_photo(chat_id, file_path):
     url = f"{BASE_URL}/sendPhoto"
-    with open(file_path, "rb") as photo:
-        files = {"photo": photo}
-        data = {"chat_id": chat_id, "caption": caption}
-        return requests.post(url, data=data, files=files).json()
+    try:
+        with open(file_path, "rb") as photo:
+            files = {"photo": photo}
+            data = {"chat_id": chat_id}
+            r = requests.post(url, data=data, files=files)
+            r.raise_for_status()
+            return r.json()
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except Exception as e:
+        print("Error in send_photo:", e)
 
-@app.route("/", methods=["POST"])
-def webhook():
-    update = request.get_json()
+def main():
+    offset = 0
+    print("Bot is running...")
+    if 1>0:
+        updates = get_updates(offset)
 
-    message = update.get("message", {})
-    chat = message.get("chat", {})
-    chat_id = chat.get("id")
-    text = message.get("text", "")
+        for update in updates.get("result", []):
+            offset = update["update_id"] + 1
 
-    if text == "start":
-        send_photo(chat_id, "test.jpg", "این فایل test.jpg است")
+            message = update.get("message")
+            if not message:
+                continue
 
-    return "ok"
+            chat_id = message["chat"]["id"]
+            text = message.get("text", "")
+
+            # برای هر پیام، عکس را ارسال می‌کند
+            print(f"Message received from {chat_id}: {text}")
+            send_photo(chat_id, FILE_PATH)
+
+        
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    main()
+
 
 
 
